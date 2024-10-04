ebaa03780a4f2d04643a74469d353da6ba87e162
ftessmann <fernando.tessmann@gmail.com>
2024-10-04 08:19:50 -0300

    aula python 2-10-24

---
+++ CTwP/Aula_10/aula.py
@@ -0,0 +1,15 @@
+numero = int(input("Digite um numero entre 2 e 11 \n"))
+
+while numero >= 2 and numero <= 11:
+    print(f"O número é {numero}")
+    numero += 1
+print(numero)
+print("fim while")
+
+for i in range(10):
+    print(i)
+print("fim")
+
+for n in range(4, 11):
+    print(n)
+print("fim")
